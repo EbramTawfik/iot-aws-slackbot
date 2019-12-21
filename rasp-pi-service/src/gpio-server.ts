f03f70f72abc9beadaf4d1c56086aa6f8ac98d52
Ebram Tawfik <ebram.s.tawfik@gmail.com>
2019-12-20 21:28:49 -0800

    Adding mqtt

--- rasp-pi-service/src/gpio-server.ts
+++ rasp-pi-service/src/gpio-server.ts
@@ -1,22 +1,27 @@
 import { Gpio, BinaryValue } from 'onoff';
+import { connect, Client, Packet, IPublishPacket } from 'mqtt';
 import { Helper } from './Helper';
 
 
 
 
 class GpioServer {
-
+    private client: Client;
     constructor() {
         try {
 
-           // Test the Gpio Using LED
-           // var LED = new Gpio(4, 'out'); 
-           // var blinkInterval = setInterval(() => this.blinkLED(LED), 1000);
+            // Test the Gpio Using LED
+            // var LED = new Gpio(4, 'out'); 
+            // var blinkInterval = setInterval(() => this.blinkLED(LED), 1000);
 
-             const button = new Gpio(4, "in", "falling");
-             button.watch((err, value) => this.onButtonChange(err, value));
+            console.log("Service Started...");
 
-             console.log("Service Started...");
+            this.setupMQTT();
+            this.setupGPIO();
+
+            console.log("-------------------------");
+
+
         }
         catch
         {
@@ -24,23 +29,64 @@ class GpioServer {
         }
     }
 
+    private setupGPIO(): void {
+        //Button GPIO Setup
+        const button = new Gpio(4, "in", "falling");
+        button.watch((err, value) => this.onButtonChange(err, value));
 
-    private onButtonChange(err: Error | null | undefined, value: BinaryValue) {
+        console.log("GPIO Setup Done ...");
+    }
+
+    private onButtonChange(err: Error | null | undefined, value: BinaryValue): void {
         if (err) {
             throw err;
         }
+        const pub_topic = "home/switches/buttonChange";
+
         if (value === Gpio.LOW) {
             console.log("Button Pressed...");
+            this.client.publish(pub_topic, "PRESSED");
             Helper.Delay(200);
         }
 
     }
 
-    private blinkLED(LED) { 
-        if (LED.readSync() === 0) { 
-            LED.writeSync(1); 
+    private setupMQTT(): void {
+        // MQTT Details
+        const broker_address = "test.mosquitto.org";
+        const client_id = "slackbot";
+        const sub_topic = "home/switches/buttonChange";
+
+        this.client = connect(`mqtt://${broker_address}`, {
+            clientId: client_id
+        });
+
+        this.client.on('message', (topic, payload, packet) => this.mQTTMessageReceived(topic, payload, packet));
+        this.client.subscribe(sub_topic);
+
+        console.log("MQTT Setup Done ...");
+    }
+
+
+
+    private mQTTMessageReceived(topic: string, payload: Buffer, packet: Packet): void {
+        let recPacket = packet as IPublishPacket;
+
+        // console.log(JSON.stringify(packet, null, 4));
+
+        console.log("message received ", payload.toString());
+        console.log("message topic=", recPacket.topic);
+        console.log("message qos=", recPacket.qos);
+        console.log("message retain flag=", recPacket.retain);
+        console.log("-------------------------");
+    }
+
+
+    private blinkLED(LED) {
+        if (LED.readSync() === 0) {
+            LED.writeSync(1);
         } else {
-            LED.writeSync(0); 
+            LED.writeSync(0);
         }
     }
 
